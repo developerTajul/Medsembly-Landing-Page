ffbae1620e4e864f34449596a56d05092cba4439
itzJunaedRayhan <webgenesis99@gmail.com>
2022-09-03 12:29:13 +0600

    Done

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+});
+
+
+var swiper = new Swiper(".mySwiperOne", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
