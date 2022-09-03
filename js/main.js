56b755a6b2e164f2d86cd7c99f48fc16b07674eb
itzJunaedRayhan <webgenesis99@gmail.com>
2022-09-03 17:29:31 +0600

    576px size Responsive Done

--- js/main.js
+++ js/main.js
@@ -1,33 +1,46 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
+(function ($) {
+  jQuery(document).ready(function () {
+
+    //  Swiper for Carousel:
+    const swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
       },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 40,
+      breakpoints: {
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 0,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
       },
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 50,
+    });
+
+    //  Swiper For Subsribe and Newsletter Section
+    const mySwiperOne = new Swiper(".mySwiperOne", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
       },
-    },
-});
+    });
+  })
+}(jQuery));
 
 
-var swiper = new Swiper(".mySwiperOne", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+
 
