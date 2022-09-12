825bcb378d0204a2c9c8497ac38272ed7b45bd77
itzJunaedRayhan <webgenesis99@gmail.com>
2022-09-12 16:18:43 +0600

    Some modification Complete

--- js/main.js
+++ js/main.js
@@ -18,10 +18,10 @@
           spaceBetween: 20,
         },
         768: {
-          slidesPerView: 3,
+          slidesPerView: 2,
           spaceBetween: 20,
         },
-        1024: {
+        992: {
           slidesPerView: 3,
           spaceBetween: 50,
         },
