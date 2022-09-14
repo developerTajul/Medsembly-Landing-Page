6684a61c4b605dba48b89b46c93b6a1a1d6073ae
itzJunaedRayhan <webgenesis99@gmail.com>
2022-09-14 13:56:10 +0600

    Blog Section Modified

--- js/main.js
+++ js/main.js
@@ -351,3 +351,12 @@ Array.from(Selectdates).forEach(date => {
 
 
 
+//  Wow.js Animation:
+wow = new WOW({
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+})
+wow.init();
